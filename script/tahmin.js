5aefea7ef569f590c86bd5c11f8b9c1e98194315
EmreMertA <emremakdag55@gmail.com>
2021-03-15 07:45:04 +0300

    API

---
+++ script/tahmin.js
@@ -0,0 +1,33 @@
+const key = "OTG6BnF08OAzrGoaepDq32GyhOhbtD8I";
+
+const havaDurumuGetir = async (id) => {
+  const temelUrl = "http://dataservice.accuweather.com/currentconditions/v1/";
+  const sorgu = `${id}?apikey=${key}`;
+
+  const res = await fetch(temelUrl + sorgu);
+  const veri = await res.json();
+
+  return veri[0];
+};
+
+const sehirGetir = async (konum) => {
+  const temelUrl =
+    "http://dataservice.accuweather.com/locations/v1/cities/search";
+  const sorgu = `?apikey=${key}&q=${konum}`;
+  const res = await fetch(temelUrl + sorgu);
+  const veri = await res.json();
+
+  console.log(veri);
+  return veri[0];
+};
+
+sehirGetir("Denizli")
+  .then((veri) => {
+    return havaDurumuGetir(veri.Key);
+  })
+  .then((veri) => {
+    console.log(veri);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
